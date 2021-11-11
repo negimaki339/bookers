c7bbaba4a9850c92ad5c22f1af677f624328f8db
EC2 Default User <ec2-user@ip-172-31-25-35.ec2.internal>
2021-11-11 08:17:33 +0000

    bookers

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+  
+  def top
+end
+  
+  
+  def show
+      
+  end
+
+  def index
+     
+  end
+
+  def new
+     
+  end
+
+  def create
+ 　　
+  end
+
+ 
+  
+  def edit
+    
+  end
+  
+   def update
+    
+   end
+  
+  def destroy
+    
+  end
+  
+end
