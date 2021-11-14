fcc082121985975e8fe90141243ca98d781a9901
EC2 Default User <ec2-user@ip-172-31-25-35.ec2.internal>
2021-11-14 08:37:37 +0000

    test5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,52 +1,46 @@
 class BooksController < ApplicationController
-  def new
-     @book = Book.new
-  end
-
-  
-  
-  
+ 
   
   def show
     @book = Book.find(params[:id])
   end
 
   def index
-      @book = Book.all
+      @books = Book.all
+      @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-    redirect_to books_path(book.id)
-  else
-    render :new
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
-  
-end
-
  
 
    def edit
     @book = Book.find(params[:id])
-    
    end
-  def update
+   
+   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to books_path(book.id)
-  end
-  def destroy
+   end
+   
+   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
-  end
+   end
   
   private
-  def book_params
+  
+   def book_params
     params.require(:book).permit(:title, :body)
-  end
-  
- 
-  
+   end
+   
 end
